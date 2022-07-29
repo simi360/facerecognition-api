6b52c9400a51e4e0724fe9ef8dd8d28363f2fe57
Harsimran Kaur <infamously13@gmail.com>
2022-07-28 21:05:42 -0400

    Adding files

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+const register = require ('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'harsimrankaur',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	db.select('*').from('users')
+	.then(data => res.json(data))
+})
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+
+app.listen(3000, () => {
+	console.log('all good');
+});
